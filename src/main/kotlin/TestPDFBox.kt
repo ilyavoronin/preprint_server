fd72d6d0cb33631696b34b7a51c131d98f3db720
Ilya Voronin <voroninilya115@gmail.com>
2020-03-21 22:34:54 +0300

    add ReferenceExtractorObject

--- src/main/kotlin/TestPDFBox.kt
+++ src/main/kotlin/TestPDFBox.kt
@@ -5,140 +5,6 @@ import java.io.File
 import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
-fun extractReferences(text : String, pageWidth : Double) : List <String> {
-    //find where references begin
-    var ind = text.lastIndexOfAny(listOf("References\\\$\\", "REFERENCES\\\$\\"), text.lastIndex, ignoreCase = false)
-    if (ind == -1) {
-        ind = text.lastIndexOfAny(listOf("References", "REFERENCES"), text.lastIndex, ignoreCase = false)
-        if (ind == -1) {
-            val indOfFirstRef = text.lastIndexOfAny(listOf("[1]"), text.lastIndex, ignoreCase = false)
-            if (indOfFirstRef == -1) {
-                return listOf()
-            }
-            ind = text.lastIndexOf("@d", indOfFirstRef)
-            ind = text.lastIndexOf("@d", ind - 1) - 1
-        }
-    }
-    //removing mark for bold text
-    var refs =  text.substring(ind).replace("\\\$\\", "");
-    //find the end of references
-    val indBeg = refs.indexOf('\n') + 1
-    var indEnd = refs.indexOfAny(listOf("\n\n", "\\%\\"), indBeg) // \\%\\ indicates a big space between lines
-    if (indEnd == -1) {
-        indEnd = refs.lastIndex
-    }
-    refs = refs.substring(indBeg, indEnd)
-    println(refs)
-    //remove page number and empty lines
-    val indentRegex = """@d[0-9.]+@d""".toRegex()
-    val regexpPageNumber = ("""($indentRegex)?\s*(\d{1,3})?\s*""").toRegex()
-    val linesWithInd = refs.lines().filter {line ->
-        !line.matches(regexpPageNumber) && !line.contains("\\%p")
-    }.map {line ->
-        val match = "^$indentRegex".toRegex().find(line) ?: return@map Pair(0.0, line)
-        Pair(match.value.drop(2).dropLast(2).toDouble(), line.drop(match.range.last + 1))
-    }
-
-    val refList = mutableListOf<String>()
-
-    var firstIndent = linesWithInd[0].first
-    var otherIndent = -1.0
-    for ((indent, _) in linesWithInd) {
-        if (abs(indent - firstIndent) > 2) {
-            otherIndent = indent
-            break
-        }
-    }
-
-    fun isFirstRefLine(line : String, firsLine : String) : Boolean {
-        if (firsLine[0] == '[') {
-            if (line[0] != '[') {
-                return false
-            }
-        }
-        if (firsLine[0].isDigit()) {
-            if (!line[0].isDigit()) {
-                return false
-            }
-        }
-        if (firsLine[0].isLetter()) {
-            if (!line[0].isLetter()) {
-                return false
-            }
-        }
-        return true
-    }
-
-    //parse references
-    if (otherIndent == -1.0) {
-        otherIndent = firstIndent
-    }
-    var curRef = ""
-    var lastRefLineIndex = 0
-    println("$firstIndent $otherIndent")
-    for((i, pair) in linesWithInd.withIndex()) {
-        val (indent, line) = pair
-        if (i == 0) {
-            curRef = line
-            continue
-        }
-        if (abs(indent - firstIndent) < 2 && isFirstRefLine(line, linesWithInd[0].second)) {
-            refList.add(curRef.replace(indentRegex, ""))
-            curRef = line
-            lastRefLineIndex = i
-        } else {
-            println("$indent $otherIndent")
-            if (abs(indent - otherIndent) < 2) {
-                if (curRef.isNotEmpty() && curRef.last() == '-') {
-                    curRef = curRef.dropLast(1)
-                    curRef += line
-                } else {
-                    curRef += " $line"
-                }
-            } else {
-                val patternChanged = ((indent < firstIndent && abs(indent - firstIndent) < 10)
-                        || indent > pageWidth / 2 || firstIndent > pageWidth / 2 && indent < pageWidth / 5)
-                if (patternChanged && isFirstRefLine(line, linesWithInd[0].second)) {
-                    if (indent > pageWidth / 2 || firstIndent > pageWidth / 2 && indent < pageWidth / 5) {
-                        otherIndent = indent + otherIndent - firstIndent
-                    }
-                    firstIndent = indent
-                    refList.add(curRef.replace(indentRegex, ""))
-                    curRef = line
-                    lastRefLineIndex = i
-                } else {
-                    println(i)
-                    //pattern has ended
-                    break
-                }
-            }
-        }
-    }
-    if (curRef.isNotEmpty()) {
-        refList.add(curRef.replace(indentRegex, ""))
-    }
-    return refList
-    refs = linesWithInd.joinToString(separator = "\n", prefix = "\n") {(intend, string) -> string.replace(indentRegex, "")}
-
-    //parse references if previous method didn't work
-    /*val regexpFirstType = """\n\[[\w-]+][\s\S]*?\n\[""".toRegex()
-    if (regexpFirstType.find(refs) != null) {
-        var i = 0
-        while (true) {
-            val match = regexpFirstType.find(refs, i) ?: break
-            i = match.range.last - 1
-            val ref = match.value.lines().dropLast(1).drop(1).joinToString(separator = " ")
-            refList += ref
-        }
-        refList += refs.substring(i + 1).lines()
-            .dropLastWhile{it.matches("""\s*""".toRegex())}.joinToString(separator = " ")
-        return refList
-    }
-
-     */
-    return refList
-}
-
 fun main() {
 
     println(measureTimeMillis {
@@ -150,7 +16,7 @@ fun main() {
             val doc = PDDocument.load(inputFile)
             val pageWidth = doc.pages[0].mediaBox.width.toDouble()
             val text = pdStripper.getText(doc)
-            val refs = extractReferences(text, pageWidth)
+            val refs = ReferenceExtractor.extract(text, pageWidth)
             outputFile.writeText("")
             refs.forEach { outputFile.appendText(it + "\n") }
             println(fileName)
--- src/main/kotlin/test_files.kt
+++ src/main/kotlin/test_files.kt
@@ -3,5 +3,33 @@ package testpdf
 val prefix = "files/"
 
 val test_files = listOf(
+    "astr1992.pdf",
+    "astr1995.pdf",
+    "astr2003(big).pdf",
+    "astr2010.pdf",
+    "astr2020.pdf",
+    "cs2005.pdf",
+    "cs2006.pdf",
+    "cs2008.pdf",
+    "cs2012(2c).pdf",
+    "cs2013(2c).pdf",
+    "cs2016.pdf",
+    "cs2018.pdf",
+    "cs2020.pdf",
+    "nuc1992.pdf",
+    "nuc1995.pdf",
+    "nuc1999.pdf",
+    "nuc1999(2).pdf",
+    "nuc2003.pdf",
+    "nuc2007.pdf",
+    "nuc2014.pdf",
+    "nuc2018.pdf",
+    "nuc2020.pdf",
+    "ph1996.pdf",
+    "ph2020.pdf",
+    "ph2005.pdf",
+    "ph2009.pdf",
+    "ph2013.pdf",
+    "ph2017.pdf",
     "ph2020.pdf"
 )
