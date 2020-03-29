9ebdac89efcb39a2fed01d85dcf8f68ef34342a6
Ilya Voronin <voroninilya115@gmail.com>
2020-03-29 13:25:15 +0300

    add missing fields to ArxivData and refactor change toString method

--- src/main/kotlin/arxiv/ArxivData.kt
+++ src/main/kotlin/arxiv/ArxivData.kt
@@ -13,24 +13,60 @@ data class ArxivData(val identifier : String) : Data {
     val specs = mutableListOf<String>()
     var creationDate = ""
     var lastUpdateDate : String? = null
-    val authors = mutableListOf<Author>()
     var title = ""
+    val authors = mutableListOf<Author>()
     var categories = mutableListOf<String>()
     var comments : String? = null
-    var mscClass : String? = null
+    var reportNo : String? = null
     var journalRef : String? = null
+    var mscClass : String? = null
+    var acmClass : String? = null
     var doi : String? = null
     var license = ""
     var abstract = ""
+
     override fun toString(): String {
-        return """id: $id
-Creation date: $creationDate
-Authors:
-${authors.foldIndexed(""){i, acc, author -> "$acc${i + 1}) ${author.name}\n"}}
-Abstract:
-$abstract
-References:
-${refList.foldIndexed("") { i, acc, ref -> "$acc${i + 1}) $ref\n" }}
-        """
+        var res = ""
+        res += "id : $id\n"
+
+        res += "Creation date: $creationDate\n"
+
+        if (lastUpdateDate != null) {
+            res += "Update date: $lastUpdateDate\n"
+        }
+
+        res += "Title: $title\n"
+
+        res += "Authors:\n"
+        res += authors.foldIndexed("") { i, acc, author -> "  $acc${i + 1}) ${author.name}\n" }
+
+        res += "Categories:\n"
+        res += categories.foldIndexed("") { i, acc, category -> "  $acc${i + 1}) ${category}\n" }
+        if (comments != null) {
+            res += "Comments:\n$comments\n"
+        }
+        if (reportNo != null) {
+            res += "Report number: $reportNo\n"
+        }
+        if (journalRef != null) {
+            res += "Journal reference: $journalRef\n"
+        }
+        if (mscClass != null) {
+            res += "MSC class: $mscClass\n"
+        }
+        if (acmClass != null) {
+            res += "ACM class: $acmClass\n"
+        }
+        if (doi != null) {
+            res += "DOI: $doi\n"
+        }
+        res += "License: $license\n"
+
+        res += "PDF url: $pdfUrl\n"
+
+        res += "References:\n"
+        res += refList.foldIndexed("") { i, acc, reference -> "  $acc${i + 1}) ${reference}\n" }
+
+        return res + "\n\n"
     }
 }
