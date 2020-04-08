4564418230221091adf03e85914a09f1fe000ae7
Ilya Voronin <voroninilya115@gmail.com>
2020-04-08 16:51:46 +0300

    fix cypher queries

--- src/main/kotlin/com/preprint/server/neo4j/DatabaseHandler.kt
+++ src/main/kotlin/com/preprint/server/neo4j/DatabaseHandler.kt
@@ -19,10 +19,10 @@ class DatabaseHandler(
     fun storeArxivData(arxivRecords : List<ArxivData>) {
         logger.info("Begin storing ${arxivRecords.size} records to the database")
 
-        val publications = mapOf("publications" to arxivRecords.map {arxivDataToMap(it)})
-
         driver.session().use {
             //create new or update publication node
+
+            val publications = mapOf("publications" to arxivRecords.map {arxivDataToMap(it)})
             it.run("""
                     UNWIND ${"$"}publications as pubData
                     MERGE (pub:${DBLabels.PUBLICATION.str} {arxivId : pubData.arxivId}) 
@@ -35,53 +35,70 @@ class DatabaseHandler(
             arxivRecords.forEach { record ->
                 //create publication -> author connection and author -> affiliation connection
                 record.authors.forEach {author ->
+                    val params = mapOf(
+                        "name" to author.name,
+                        "aff" to author.affiliation,
+                        "recordId" to record.id
+                    )
                     it.run("""
-                        MERGE (auth:${DBLabels.AUTHOR.str} {name: "${author.name}"})
+                        MERGE (auth:${DBLabels.AUTHOR.str} {name: ${parm("name")}})
                         ${if (author.affiliation != null) {
                         """
-                            MERGE (aff:${DBLabels.AFFILIATION.str} {name: "${author.affiliation}"})
+                            MERGE (aff:${DBLabels.AFFILIATION.str} {name: ${parm("aff")}})
                             MERGE (auth)-[:${DBLabels.WORKS.str}]->(aff)
                         """.trimIndent()
                         } else ""
                         }
                         WITH auth
-                        MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: "${record.id}"})
+                        MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("recordId")}})
                         MERGE (pub)-[:${DBLabels.AUTHORED.str}]->(auth)
-                    """.trimIndent())
+                    """.trimIndent(), params)
                 }
                 logger.info("Publication->author connections created")
 
                 //create publication -> publication connections
                 record.refList.forEach {ref ->
+                    val params = mapOf(
+                        "rid" to record.id,
+                        "arxId" to ref.arxivId,
+                        "rdoi" to ref.doi,
+                        "rtit" to ref.title,
+                        "rRef" to ref.rawReference
+                    )
                     val res = it.run("""
-                        MATCH (pubFrom:${DBLabels.PUBLICATION.str} {arxivId: "${record.id}"})
+                        MATCH (pubFrom:${DBLabels.PUBLICATION.str} {arxivId: ${parm("rid")}})
                         MATCH (pubTo:${DBLabels.PUBLICATION.str})
-                        WHERE pubTo.arxivId = "${ref.arxivId}" OR pubTo.doi = "${ref.doi}" OR pubTo.title = "${ref.title}"
-                        MERGE (pubFrom)-[c:${DBLabels.CITES.str} {rawRef: "${ref.rawReference}"}]->(pubTo)
+                        WHERE pubTo.arxivId = ${parm("arxId")} OR
+                            pubTo.doi = ${parm("rdoi")} OR pubTo.title = ${parm("rtit")}
+                        MERGE (pubFrom)-[c:${DBLabels.CITES.str} {rawRef: ${parm("rRef")}}]->(pubTo)
                         RETURN pubTo
-                    """.trimIndent())
+                    """.trimIndent(), params)
                     if (res.list().size == 0) {
                         //then the cited publication doesn't exist in database
                         //crete missing publication -> publication connection
                         it.run("""
-                            MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: "${record.id}"})
-                            MERGE (mpub:${DBLabels.MISSING_PUBLICATION.str} {title: "${ref.title}"})
+                            MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("rid")}})
+                            MERGE (mpub:${DBLabels.MISSING_PUBLICATION.str} {title: ${parm("rtit")}})
                             MERGE (mpub)-[c:${DBLabels.CITED_BY.str}]->(pub)
-                            SET c.rawRef = "${ref.rawReference}"
-                            ${if (ref.arxivId != null) """mpub.arxivId = "${ref.arxivId}"""" else ""}
-                            ${if (ref.doi != null) """mpub.doi = "${ref.doi}"""" else ""}
-                        """.trimIndent())
+                            SET c.rawRef = ${parm("rRef")},
+                                ${if (ref.arxivId != null) """mpub.arxivId = ${parm("arxId")},""" else ""}
+                                ${if (ref.doi != null) """mpub.doi = ${parm("rdoi")}""" else ""}
+                        """.trimIndent(), params)
                     }
                 }
                 logger.info("Publication->publication connections created")
 
                 //create publication -> journal connections
+                val params = mapOf(
+                    "arxId" to record.id,
+                    "rjrl" to record.journalRef
+                )
                 if (record.journalRef != null) {
                     it.run("""
-                       MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: "${record.id}"})
-                       MERGE (j:${DBLabels.JOURNAL.str} {title: "${record.journalRef}"})
+                       MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("arxId")}})
+                       MERGE (j:${DBLabels.JOURNAL.str} {title: ${parm("rjrl")}})
                        MERGE (pub)-[jref:${DBLabels.PUBLISHED_IN}]->(j)
-                    """.trimIndent())
+                    """.trimIndent(), params)
                 }
                 logger.info("Publication->journal connections created")
             }
@@ -105,6 +122,10 @@ class DatabaseHandler(
             res += "acmClass" to record.acmClass!!
         }
         return res
+    }
+
+    fun parm(paramName : String) : String {
+        return "\$$paramName"
     }
 
     override fun close() {
