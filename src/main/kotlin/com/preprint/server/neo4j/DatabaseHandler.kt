9f46a165bee2d3a8b9782336bff8d2e7cb6e8865
Ilya Voronin <voroninilya115@gmail.com>
2020-04-10 21:18:16 +0300

    refactor DatabaseHandler

--- src/main/kotlin/com/preprint/server/neo4j/DatabaseHandler.kt
+++ src/main/kotlin/com/preprint/server/neo4j/DatabaseHandler.kt
@@ -4,6 +4,7 @@ import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.GraphDatabase
 import com.preprint.server.arxiv.ArxivData
 import org.apache.logging.log4j.kotlin.logger
+import org.neo4j.driver.Session
 import java.io.Closeable
 
 class DatabaseHandler(
@@ -21,7 +22,6 @@ class DatabaseHandler(
 
         driver.session().use {
             //create new or update publication node
-
             val publications = mapOf("publications" to arxivRecords.map {arxivDataToMap(it)})
             it.run("""
                     UNWIND ${"$"}publications as pubData
@@ -33,79 +33,13 @@ class DatabaseHandler(
             logger.info("Publication nodes created")
 
             arxivRecords.forEach { record ->
-                //create publication -> author connection and author -> affiliation connection
-                record.authors.forEach {author ->
-                    val params = mapOf(
-                        "name" to author.name,
-                        "aff" to author.affiliation,
-                        "recordId" to record.id
-                    )
-                    it.run("""
-                        MERGE (auth:${DBLabels.AUTHOR.str} {name: ${parm("name")}})
-                        ${if (author.affiliation != null) {
-                        """
-                            MERGE (aff:${DBLabels.AFFILIATION.str} {name: ${parm("aff")}})
-                            MERGE (auth)-[:${DBLabels.WORKS.str}]->(aff)
-                        """.trimIndent()
-                        } else ""
-                        }
-                        WITH auth
-                        MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("recordId")}})
-                        MERGE (pub)-[:${DBLabels.AUTHORED.str}]->(auth)
-                    """.trimIndent(), params)
-                }
-                logger.info("Publication->author connections created")
+                createAuthorConnections(it, record)
 
-                //create publication -> publication connections
-                record.refList.forEach {ref ->
-                    val params = mapOf(
-                        "rid" to record.id,
-                        "arxId" to ref.arxivId,
-                        "rdoi" to ref.doi,
-                        "rtit" to ref.title,
-                        "rRef" to ref.rawReference
-                    )
-                    val res = it.run("""
-                        MATCH (pubFrom:${DBLabels.PUBLICATION.str} {arxivId: ${parm("rid")}})
-                        MATCH (pubTo:${DBLabels.PUBLICATION.str})
-                        WHERE pubTo <> pubFrom AND (pubTo.arxivId = ${parm("arxId")} OR
-                            pubTo.doi = ${parm("rdoi")} OR pubTo.title = ${parm("rtit")})
-                        MERGE (pubFrom)-[c:${DBLabels.CITES.str} {rawRef: ${parm("rRef")}}]->(pubTo)
-                        RETURN pubTo
-                    """.trimIndent(), params)
+                createCitationsConnections(it, record)
 
-                    if (res.list().size == 0 && ref.title != null) {
-                        //then the cited publication doesn't exist in database
-                        //crete missing publication -> publication connection
-                        it.run("""
-                            MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("rid")}})
-                            MERGE (mpub:${DBLabels.MISSING_PUBLICATION.str} {title: ${parm("rtit")}})
-                            MERGE (mpub)-[c:${DBLabels.CITED_BY.str}]->(pub)
-                            SET c.rawRef = ${parm("rRef")}
-                                ${if (ref.arxivId != null) """,mpub.arxivId = ${parm("arxId")}""" else ""}
-                                ${if (ref.doi != null) """,mpub.doi = ${parm("rdoi")}""" else ""}
-                        """.trimIndent(), params)
-                    }
-                }
-                logger.info("Publication->publication connections created")
-
-                //create publication -> journal connections
-                val params = mapOf(
-                    "arxId" to record.id,
-                    "rjrl" to record.journalRef
-                )
-                if (record.journalRef != null) {
-                    it.run("""
-                       MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("arxId")}})
-                       MERGE (j:${DBLabels.JOURNAL.str} {title: ${parm("rjrl")}})
-                       MERGE (pub)-[jref:${DBLabels.PUBLISHED_IN}]->(j)
-                    """.trimIndent(), params)
-                }
-                logger.info("Publication->journal connections created")
+                createJournalPublicationConnections(it, record)
             }
-
-            //create publication author connections
-
+            logger.info("All connections created")
         }
     }
 
@@ -127,6 +61,83 @@ class DatabaseHandler(
 
     fun parm(paramName : String) : String {
         return "\$$paramName"
+    }
+
+    //create publication -> author connection and author -> affiliation connection
+    private fun createAuthorConnections(session : Session, record: ArxivData) {
+        record.authors.forEach {author ->
+            val params = mapOf(
+                "name" to author.name,
+                "aff" to author.affiliation,
+                "recordId" to record.id
+            )
+            val createAffiliationQuery =
+                if (author.affiliation != null) {
+                    """
+                        MERGE (aff:${DBLabels.AFFILIATION.str} {name: ${parm("aff")}})
+                        MERGE (auth)-[:${DBLabels.WORKS.str}]->(aff)
+                    """.trimIndent()
+                } else ""
+
+            session.run("""
+                        MERGE (auth:${DBLabels.AUTHOR.str} {name: ${parm("name")}})
+                        $createAffiliationQuery
+                        WITH auth
+                        MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("recordId")}})
+                        MERGE (pub)-[:${DBLabels.AUTHORED.str}]->(auth)
+                    """.trimIndent(), params)
+        }
+    }
+
+    //create publication -> publication connections and create MissingPublication nodes
+    private fun createCitationsConnections(session : Session, record : ArxivData) {
+        record.refList.forEach {ref ->
+            val params = mapOf(
+                "rid" to record.id,
+                "arxId" to ref.arxivId,
+                "rdoi" to ref.doi,
+                "rtit" to ref.title,
+                "rRef" to ref.rawReference
+            )
+            val res = session.run("""
+                        MATCH (pubFrom:${DBLabels.PUBLICATION.str} {arxivId: ${parm("rid")}})
+                        MATCH (pubTo:${DBLabels.PUBLICATION.str})
+                        WHERE pubTo <> pubFrom AND (pubTo.arxivId = ${parm("arxId")} OR
+                            pubTo.doi = ${parm("rdoi")} OR pubTo.title = ${parm("rtit")})
+                        MERGE (pubFrom)-[c:${DBLabels.CITES.str} {rawRef: ${parm("rRef")}}]->(pubTo)
+                        RETURN pubTo
+                    """.trimIndent(), params)
+
+            if (res.list().size == 0 && ref.title != null) {
+                //then the cited publication doesn't exist in database
+                //crete missing publication -> publication connection
+                val searchByArxivIdQuery = {if (ref.arxivId != null) """,mpub.arxivId = ${parm("arxId")}""" else ""}
+                val searchByDoiQuery = {if (ref.doi != null) """,mpub.doi = ${parm("rdoi")}""" else ""}
+                session.run("""
+                            MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("rid")}})
+                            MERGE (mpub:${DBLabels.MISSING_PUBLICATION.str} {title: ${parm("rtit")}})
+                            MERGE (mpub)-[c:${DBLabels.CITED_BY.str}]->(pub)
+                            SET c.rawRef = ${parm("rRef")}
+                                $searchByArxivIdQuery
+                                $searchByDoiQuery
+                        """.trimIndent(), params)
+            }
+        }
+    }
+
+    //create publication -> journal connections
+    private fun createJournalPublicationConnections(session: Session, record: ArxivData) {
+        val params = mapOf(
+            "arxId" to record.id,
+            "rjrl" to record.journalRef
+        )
+        if (record.journalRef != null) {
+            session.run("""
+                       MATCH (pub:${DBLabels.PUBLICATION.str} {arxivId: ${parm("arxId")}})
+                       MERGE (j:${DBLabels.JOURNAL.str} {title: ${parm("rjrl")}})
+                       MERGE (pub)-[jref:${DBLabels.PUBLISHED_IN}]->(j)
+                    """.trimIndent(), params)
+        }
     }
 
     override fun close() {
