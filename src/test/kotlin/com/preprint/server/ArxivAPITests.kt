860341a8e81e87996ab57a5730c6e1e8327c0f9c
Ilya Voronin <voroninilya115@gmail.com>
2020-04-13 20:02:49 +0300

    add test for request with date

--- src/test/kotlin/com/preprint/server/ArxivAPITests.kt
+++ src/test/kotlin/com/preprint/server/ArxivAPITests.kt
@@ -9,18 +9,28 @@ import com.preprint.server.arxiv.ArxivData
 import com.preprint.server.arxiv.ArxivXMLParser
 import io.mockk.*
 import org.apache.logging.log4j.kotlin.KotlinLogger
+import org.junit.jupiter.api.AfterEach
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 
 class ArxivAPITests {
 
-    @Test
-    fun testGetBulkApiRecordsWithResumptionTokenSuccess() {
+    @BeforeEach
+    fun initTest() {
         mockkConstructor(KotlinLogger::class)
         every {anyConstructed<KotlinLogger>().info(any<String>())} just Runs
-        val spyArxiApi = spyk(ArxivAPI)
+    }
 
+    @AfterEach
+    fun clear() {
+        unmockkAll()
+    }
+
+    @Test
+    fun testGetBulkApiRecordsWithResumptionTokenSuccess() {
+        val spyArxiApi = spyk(ArxivAPI)
         val requestMock = mockk<Request>()
         val resultMock = mockk<Result.Success<String>>()
         val responseMock = mockk<Response>()
@@ -47,7 +57,36 @@ class ArxivAPITests {
         assertEquals("pdf url id1", arxivRecords?.get(0)?.pdfUrl)
         assertEquals("new token", newResToken)
         assertEquals(1000, recTotal)
+    }
 
-        unmockkAll()
+    @Test
+    fun testGetBulkApiRecordsWithDateSuccess() {
+        val spyArxiApi = spyk(ArxivAPI)
+        val requestMock = mockk<Request>()
+        val resultMock = mockk<Result.Success<String>>()
+        val responseMock = mockk<Response>()
+
+        val url = ArxivAPI.requestBulkUrlPrefix + "verb=ListRecords&from=2020-01-10&metadataPrefix=arXiv"
+        mockkStatic("com.github.kittinunf.fuel.FuelKt")
+        every { url.httpGet().timeoutRead(any()).responseString() }returns
+                Triple(requestMock, responseMock, resultMock)
+
+        every { resultMock.get()} returns "xml text"
+
+        mockkObject(ArxivXMLParser)
+        every {ArxivXMLParser.parseArxivRecords("xml text")} returns
+                Triple(listOf(ArxivData("ident", id = "id1")), "new token", 1000)
+        val slot = slot<List<String>>()
+        every { spyArxiApi.getRecordsLinks(capture(slot))} answers {listOf("pdf url ${slot.captured[0]}")}
+
+
+        val (arxivRecords, newResToken, recTotal) =
+            spyArxiApi.getBulkArxivRecords("2020-01-10", "", 100)
+
+        assertTrue(arxivRecords?.size == 1)
+        assertEquals("id1", arxivRecords?.get(0)?.id)
+        assertEquals("pdf url id1", arxivRecords?.get(0)?.pdfUrl)
+        assertEquals("new token", newResToken)
+        assertEquals(1000, recTotal)
     }
 }
