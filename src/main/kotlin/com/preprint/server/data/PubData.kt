129a799804c5132c2ebf7ac0434be74200565d14
Ilya Voronin <voroninilya115@gmail.com>
2020-04-19 15:16:26 +0300

    add some properties from ArxivData to PubData interface

--- src/main/kotlin/com/preprint/server/data/PubData.kt
+++ src/main/kotlin/com/preprint/server/data/PubData.kt
@@ -3,7 +3,12 @@ package com.preprint.server.data
 import com.preprint.server.ref.Reference
 
 interface PubData {
-    var id : String
-    var pdfUrl : String
+    var abstract : String
+    var year : String?
+    var title : String
+    val authors : MutableList<Author>
+    var journalRef : String?
+    var doi : String?
     var refList : MutableList<Reference>
+    var pdfUrl : String
 }
