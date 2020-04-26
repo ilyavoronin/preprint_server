1b4040a6fcbe5719718b5f5c6590e25d1047cb58
Ilya Voronin <voroninilya115@gmail.com>
2020-04-26 15:00:14 +0300

    add exception handling for CrossRefValidator

--- core/src/main/kotlin/com/preprint/server/crossref/CrossRefApi.kt
+++ core/src/main/kotlin/com/preprint/server/crossref/CrossRefApi.kt
@@ -6,9 +6,11 @@ import com.github.kittinunf.result.Result
 import com.preprint.server.Config
 import com.preprint.server.arxiv.ArxivAPI
 import com.preprint.server.utils.RequestLimiter
+import org.apache.logging.log4j.kotlin.logger
 import java.net.URLEncoder
 
 object CrossRefApi {
+    val logger = logger()
     const val prefix = "https://api.crossref.org"
     val email = Config.config["email"].toString()
     val maxRecordsNumber = 5
@@ -17,7 +19,12 @@ object CrossRefApi {
     fun findRecord(ref : String) : List<CRData> {
         reqLimiter.waitForRequest()
         val url = "$prefix/works?query=${URLEncoder.encode(ref, "utf-8")}&rows=$maxRecordsNumber&mailto=$email"
-        val (_, response, result) = url.httpGet().timeoutRead(10000).responseString()
+        val (_, response, result) = try {
+            url.httpGet().timeoutRead(10000).responseString()
+        } catch (e : Exception) {
+            logger.error("Failed to validate: ${e.message}")
+            throw ApiRequestFailedException(e.message)
+        }
         val (newLimit, newInterval) = getNewInterval(response)
         reqLimiter.set(newLimit, newInterval)
         when (result) {
--- core/src/main/kotlin/com/preprint/server/validation/Validator.kt
+++ core/src/main/kotlin/com/preprint/server/validation/Validator.kt
@@ -6,13 +6,20 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.apache.logging.log4j.kotlin.logger
+import java.lang.Exception
 
 interface Validator {
     suspend fun validate(refList : List<Reference>) = withContext(Dispatchers.IO) {
         logger().info("Begin validation of ${refList.size} references")
         val jobs = mutableListOf<Job>()
         for (ref in refList) {
-            jobs.add(launch { validate(ref) })
+            jobs.add(launch {
+                try {
+                    validate(ref)
+                } catch (e : Exception) {
+                    println("Failed to validate ${ref.rawReference}:\n ${e.message}")
+                }
+            })
         }
         jobs.forEach {it.join()}
     }
