09975a6acc5bc3145292fdaf21d4f33d9b227510
Ilya Voronin <voroninilya115@gmail.com>
2020-04-26 16:52:40 +0300

    add xml parser for arxiv manifest

---
+++ arxiv-s3/src/main/kotlin/com/preprint/server/arxivs3/ManifestParser.kt
@@ -0,0 +1,42 @@
+package com.preprint.server.arxivs3
+
+import org.xml.sax.Attributes
+import org.xml.sax.helpers.DefaultHandler
+import java.io.File
+import javax.xml.parsers.SAXParser
+import javax.xml.parsers.SAXParserFactory
+
+object ManifestParser {
+    lateinit var parser: SAXParser
+    fun parseFilenames(path : String) : List<String> {
+        val file = File(path)
+        val factory = SAXParserFactory.newInstance()
+        parser = factory.newSAXParser()
+
+        val handler = object: DefaultHandler() {
+            val filenames = mutableListOf<String>()
+            var bFilename = false
+            override fun startElement(uri: String?, localName: String?, qName: String?, attributes: Attributes?) {
+                if (qName.equals("filename")) {
+                    bFilename = true
+                }
+            }
+
+            override fun endElement(uri: String?, localName: String?, qName: String?) {
+                if (qName.equals("filename")) {
+                    bFilename = false
+                }
+            }
+
+            override fun characters(ch: CharArray, start: Int, length: Int) {
+                if (bFilename) {
+                    filenames.add(String(ch, start, length).trim())
+                }
+            }
+        }
+
+        parser.parse(File(path), handler)
+
+        return handler.filenames
+    }
+}
