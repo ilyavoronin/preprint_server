9c2d9b44614697b16b77c3350d5025b10c7abed4
Ilya Voronin <voroninilya115@gmail.com>
2020-05-03 21:18:06 +0300

    ArxivCollector, refactor and comments

--- core/src/main/kotlin/com/preprint/server/arxiv/ArxivCollector.kt
+++ core/src/main/kotlin/com/preprint/server/arxiv/ArxivCollector.kt
@@ -9,42 +9,71 @@ import com.preprint.server.validation.CrossRefValidator
 import org.apache.logging.log4j.kotlin.logger
 import java.lang.Thread.sleep
 
+
+/**
+ * Used to collect data about arxiv publications
+ */
 object ArxivCollector {
     val logger = logger()
     var resumptionToken = ""
-    const val limit = 100
-    var sleepTime : Long = 600000
+
+    //the number of records to get from each request
+    var limit = 1000
+
+    //the time to sleep when arxiv API request fails
+    var sleepTime: Long = 600000
+
+
+    /**
+     * Collects all data sequentially from the `startDate`,
+     * get references for all received records
+     * (downloads pdf for each record and extracts references),
+     * and then stores all received data into the database
+     *
+     * If `resumptionToken` is empty then makes request with the given,
+     * otherwise make requests with given resumption token to the arxiv api
+     * and 'startDate` will be ignored by the ArxivApi object later
+     * (read more about working with resumption token in ArxivApi description)
+     */
     fun collect(
-        startDate : String,
-        dbHandler : DatabaseHandler,
-        resumptionToken_ : String = ""
+        startDate: String,
+        dbHandler: DatabaseHandler,
+        resumptionToken_: String = ""
     ) {
         var recordsProcessed = 0
-        logger.info("Begin collecting arxiv metadata from $startDate with resumption token:$resumptionToken")
         resumptionToken = resumptionToken_
+
+        if (resumptionToken_ == "") {
+            logger.info("Begin collecting arxiv metadata from $startDate")
+        } else {
+            logger.info("Continue collecting arxiv metadata from $startDate with resumption token:$resumptionToken")
+        }
+
+        //do request until resumption token in the response will be empty,
+        //that means that this was the last pack of records
         do {
-            try {
-                val (newArxivRecords, newResumptionToken, recordsTotal) =
-                    ArxivAPI.getBulkArxivRecords(startDate, resumptionToken, limit)
-
-                resumptionToken = newResumptionToken
-
-                PdfHandler.getFullInfo(
-                    newArxivRecords,
-                    "files/",
-                    CustomReferenceExtractor,
-                    listOf(CrossRefValidator, ArxivValidator),
-                    false
-                )
-
-                dbHandler.storeArxivData(newArxivRecords)
-
-                recordsProcessed += newArxivRecords.size
-                logger.info("Records processed ${recordsProcessed} out of $recordsTotal")
-            } catch (e: ArxivAPI.ApiRequestFailedException) {
+            val (newArxivRecords, newResumptionToken, recordsTotal) = try {
+                ArxivAPI.getBulkArxivRecords(startDate, resumptionToken, limit)
+            }  catch (e: ArxivAPI.ApiRequestFailedException) {
                 sleep(sleepTime)
                 continue
             }
+            resumptionToken = newResumptionToken
+
+            //get references for all records, and store them
+            // in the `refList` property of each record in `newArxivRecords`
+            PdfHandler.getFullInfo(
+                newArxivRecords,
+                "files/",
+                CustomReferenceExtractor,
+                listOf(CrossRefValidator, ArxivValidator),
+                false
+            )
+
+            dbHandler.storeArxivData(newArxivRecords)
+
+            recordsProcessed += newArxivRecords.size
+            logger.info("Records processed ${recordsProcessed} out of $recordsTotal")
         } while (resumptionToken != "")
     }
 }
