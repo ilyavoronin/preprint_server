d104f781c80a850ee5a3355193643a5bddc45bbe
Ilya Voronin <voroninilya115@gmail.com>
2020-05-03 19:17:50 +0300

    ArxivS3Downloader, refactor and comments

--- arxiv-s3/src/main/kotlin/com/preprint/server/arxivs3/ArxivS3Downloader.kt
+++ arxiv-s3/src/main/kotlin/com/preprint/server/arxivs3/ArxivS3Downloader.kt
@@ -11,6 +11,12 @@ import org.apache.logging.log4j.kotlin.logger
 import java.io.File
 import java.lang.Exception
 
+
+/**
+ * Downloads archives with pdf and manifest from AmazonS3 requester pays bucket
+ * In order to work with AmazonS3 you need to specify your credentials in `~/.aws/credentials`
+ * IMPORTANT: You pay for downloading any data from AmazonS3 requester pays buckets
+ */
 object ArxivS3Downloader {
     val logger = logger()
     const val bucketName = "arxiv"
@@ -33,22 +39,31 @@ object ArxivS3Downloader {
         .build()
 
 
-    fun downloadManifest(path : String) {
+    /**
+     * Downloads arxiv's manifest.xml file into the given directory
+     */
+    fun downloadManifest(path: String) {
 
         logger.info("Begin manifest download")
         amazonS3.getObject(
-            GetObjectRequest(bucketName, manifestKey, true), File(path)
-        ) ?: throw DownloaderException("Failed to dowload")
+                GetObjectRequest(bucketName, manifestKey, true),
+                File(path)
+        ) ?: throw DownloadFailedException("Failed to dowload")
+
         logger.info("Download finished")
     }
 
+
+    /**
+     * Download archive into the given directory
+     */
     fun download(pdfKey : String, path : String) {
         logger.info("Begin $pdfKey download")
-        val pdf = amazonS3.getObject(
+        amazonS3.getObject(
             GetObjectRequest(bucketName, pdfKey, true), File(path)
-        ) ?: throw DownloaderException("Failed to download")
+        ) ?: throw DownloadFailedException("Failed to download")
         logger.info("Donwload finished")
     }
 
-    class DownloaderException(override val message : String) : Exception(message)
+    class DownloadFailedException(override val message : String) : Exception(message)
 }
