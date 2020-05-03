58d4b47e7815958f1fcc1f49cb307b36cc4091df
Ilya Voronin <voroninilya115@gmail.com>
2020-05-03 18:50:47 +0300

    ArxivS3Collector, refactor and comments

--- arxiv-s3/src/main/kotlin/com/preprint/server/arxivs3/ArxivS3Collector.kt
+++ arxiv-s3/src/main/kotlin/com/preprint/server/arxivs3/ArxivS3Collector.kt
@@ -7,7 +7,6 @@ import com.preprint.server.neo4j.DatabaseHandler
 import com.preprint.server.ref.ReferenceExtractor
 import com.preprint.server.validation.Validator
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.apache.commons.codec.digest.DigestUtils
@@ -24,16 +23,28 @@ import java.nio.file.Paths
 import java.security.MessageDigest
 import java.util.concurrent.Executors
 
-
+/**
+ * Download all arxives(in .tar format) from Amazon S3 requester pays bucket sequentially,
+ * extract references and store all data into the database.
+ */
 object ArxivS3Collector {
-    val logger = logger()
+    private val logger = logger()
     val path = Config.config["arxiv_pdf_path"].toString()
     val manifestFileName = "manifest.xml"
+
+    /**
+     * Uses previosly created dbHandler to store the data.
+     * Validators will be applied to references after extraction
+     * in the order that they stored in the list
+     * FixedThreadPool with `maxThreads` will be used during
+     * the reference extraction, or `Dispatchers.Default` from coroutines
+     * library if `maxThreads` = -1
+     */
     fun beginBulkDownload(
-        dbHandler : DatabaseHandler,
+        dbHandler: DatabaseHandler,
         referenceExtractor: ReferenceExtractor?,
-        validators : List<Validator>,
-        maxThreads : Int = -1
+        validators: List<Validator>,
+        maxThreads: Int = -1
     ) {
         File("$path/pdf/").mkdir()
         val manifestPath = "$path/$manifestFileName"
@@ -44,36 +55,53 @@ object ArxivS3Collector {
             logger.info("manifest is already downloaded")
         }
 
+        //get filenames and md5 hash of each file from manifest
         val fileNames = ManifestParser.parseFilenames(manifestPath)
         fileNames.forEach {(filename, md5sum) ->
             val pdfPath = "$path/$filename"
+
+            //download archive only if it wasn't downloaded before
             if (!File(pdfPath).exists() || !compareMD5(pdfPath, md5sum)) {
                 ArxivS3Downloader.download(filename, path + "/" + pdfPath)
             }
             else {
                 logger.info("$filename is already downloaded")
             }
+
             processFile(pdfPath, dbHandler, referenceExtractor, validators, maxThreads)
         }
     }
 
+    /**
+     * Extracts all pdf files from one archive(with `filename` name)
+     * Then extracts references from all pdf's concurrently
+     * and stored data into the database
+     */
     private fun processFile(
-        filename : String,
+        filename: String,
         dbHandler: DatabaseHandler,
         referenceExtractor: ReferenceExtractor?,
         validators: List<Validator>,
         maxThreads: Int
     ) {
+        //this directory will be used to store extracted pdf
+        //and will be deleted after processing all of them
         val outputDir = File("$path/tmp")
+        deleteDir(outputDir)
         outputDir.mkdir()
+
         try {
             val filenames = unzip(filename, outputDir)
             val ids = filenames.map {getIdFromFilename(it)}
+
+            //get metadata about each extracted pdf
             val records = ArxivAPI.getArxivRecords(ids)
+
             if (referenceExtractor != null) {
                 val dispatcher =
                     if (maxThreads == -1) Dispatchers.Default
                     else Executors.newFixedThreadPool(maxThreads).asCoroutineDispatcher()
+
                 runBlocking(dispatcher) {
                     records.zip(filenames).forEach { (record, filepath) ->
                         launch {
@@ -90,9 +118,14 @@ object ArxivS3Collector {
         }
     }
 
-    private fun unzip(input: String, outputDir : File) : List<String> {
+    /**
+     * Extracts files from tar archive(whith `inputFilePath` path) and
+     * stores all extracted files in the `outputDir`
+     * Returns list of paths to files
+     */
+    private fun unzip(inputFilePath: String, outputDir : File) : List<String> {
         val untaredFiles = mutableListOf<String>()
-        val inputStream: InputStream = FileInputStream(File(input))
+        val inputStream: InputStream = FileInputStream(File(inputFilePath))
         val debInputStream =
             ArchiveStreamFactory().createArchiveInputStream("tar", inputStream) as TarArchiveInputStream
         var entry: TarArchiveEntry? = null
@@ -108,6 +141,9 @@ object ArxivS3Collector {
         return untaredFiles
     }
 
+    /**
+     * Deleted directory and all stored files(not recursievly)
+     */
     private fun deleteDir(dir : File) {
         val files = dir.listFiles()
         for (file in files) {
@@ -116,14 +152,24 @@ object ArxivS3Collector {
         dir.delete()
     }
 
+    /**
+     * This function relies on the fact that filename
+     * is exactly the arxivId but without '/'
+     * Return valid arxiv id
+     */
     private fun getIdFromFilename(filename: String) : String {
         val fn = Paths.get(filename).fileName.toString()
         val i = fn.indexOfFirst { it.isDigit() }
         return fn.substring(0, i) + "/" + fn.substring(i).dropLast(4)
     }
 
+
+    /**
+     * Extracts references from the given file(with `filepath` path)
+     * Validates references with each validator from `validators`
+     */
     private fun getRefList(
-        filepath : String,
+        filepath: String,
         referenceExtractor: ReferenceExtractor,
         validators: List<Validator>
     ) : MutableList<Reference>{
@@ -131,12 +177,15 @@ object ArxivS3Collector {
             val refs = referenceExtractor.extractUnverifiedReferences(
                 File(filepath).readBytes()
             ).toMutableList()
+
             logger.info("Begin validation")
+
             runBlocking {
                 validators.forEach { validator ->
                     validator.validate(refs)
                 }
             }
+
             logger.info("Validated ${refs.count { it.validated }} out of ${refs.size}")
             return refs
         } catch (e : Exception) {
@@ -144,6 +193,9 @@ object ArxivS3Collector {
         }
     }
 
+    /**
+     * compares md5hash of the file with `md5sumToCompare`
+     */
     private fun compareMD5(path : String, md5sumToCompare : String) : Boolean {
         val md = DigestUtils.md5Hex(File(path).readBytes())
         return md == md5sumToCompare
