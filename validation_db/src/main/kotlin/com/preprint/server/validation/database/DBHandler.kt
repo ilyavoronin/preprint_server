3b2361a5d217a8d98835782a808ec1a887a122b7
Ilya Voronin <voroninilya115@gmail.com>
2020-05-05 01:57:54 +0300

    add more databases

--- validation_db/src/main/kotlin/com/preprint/server/validation/database/DBHandler.kt
+++ validation_db/src/main/kotlin/com/preprint/server/validation/database/DBHandler.kt
@@ -14,10 +14,16 @@ class DBHandler : AutoCloseable {
     private val titleDbPath = File(dbFolderPath, "title")
     private val jpageDbPath = File(dbFolderPath, "jpage")
     private val volPageYearDbPath = File(dbFolderPath, "volpy")
+    private val authorYDbPath = File(dbFolderPath, "authory")
+    private val authorVDbPath = File(dbFolderPath, "authorv")
+    private val authorPDbPath = File(dbFolderPath, "authorp")
     private val mainDb: RocksDB
     private val titleDb: RocksDB
     private val jpageDb: RocksDB
     private val volPageYearDb: RocksDB
+    private val authorYearDb: RocksDB
+    private val authorVolumeDb: RocksDB
+    private val authorPageDb: RocksDB
     private val options: Options
     private val logger = logger()
 
@@ -31,6 +37,9 @@ class DBHandler : AutoCloseable {
         titleDb = RocksDB.open(options, titleDbPath.absolutePath)
         jpageDb = RocksDB.open(options, jpageDbPath.absolutePath)
         volPageYearDb = RocksDB.open(options, volPageYearDbPath.absolutePath)
+        authorYearDb = RocksDB.open(options, authorYDbPath.absolutePath)
+        authorVolumeDb = RocksDB.open(options, authorVDbPath.absolutePath)
+        authorPageDb = RocksDB.open(options, authorPDbPath.absolutePath)
     }
     fun storeRecords(records: List<SemanticScholarData>) {
         var progress = 0
@@ -66,6 +75,24 @@ class DBHandler : AutoCloseable {
         return decodeIds(recordsBytes) ?: listOf()
     }
 
+    fun getByAuthorVolume(authors: String, volume: String) : List<Long> {
+        val bytes = encode(Pair(authors, volume))
+        val recordsBytes = authorVolumeDb.get(bytes) ?: return listOf()
+        return decodeIds(recordsBytes) ?: listOf()
+    }
+
+    fun getByAuthorPage(authors: String, firstPage: Int) : List<Long> {
+        val bytes = encode(Pair(authors, firstPage))
+        val recordsBytes = authorPageDb.get(bytes) ?: return listOf()
+        return decodeIds(recordsBytes) ?: listOf()
+    }
+
+    fun getByAuthorYear(authors: String, year: Int) : List<Long> {
+        val bytes = encode(Pair(authors, year))
+        val recordsBytes = authorYearDb.get(bytes) ?: return listOf()
+        return decodeIds(recordsBytes) ?: listOf()
+    }
+
     private fun storeRecord(record: SemanticScholarData) {
         val id = currentId.getAndIncrement()
         val recordBytes = Klaxon().toJsonString(record).toByteArray()
@@ -93,6 +120,30 @@ class DBHandler : AutoCloseable {
             recordList.add(id)
             volPageYearDb.put(bytes, encode(recordList.toList()))
         }
+
+        if (record.authors.size >= 3) {
+            val authorString = getFirstAuthorLetters(record)
+            if (!record.journalVolume.isNullOrBlank()) {
+                val bytes = encode(Pair(authorString, record.journalVolume))
+                val recordList = getByAuthorVolume(authorString, record.journalVolume).toMutableList()
+                recordList.add(id)
+                authorVolumeDb.put(bytes, encode(recordList.toList()))
+            }
+
+            if (record.firstPage != null) {
+                val bytes = encode(Pair(authorString, record.firstPage))
+                val recordList = getByAuthorPage(authorString, record.firstPage!!).toMutableList()
+                recordList.add(id)
+                authorPageDb.put(bytes, encode(recordList.toList()))
+            }
+
+            if (record.year != null) {
+                val bytes = encode(Pair(authorString, record.year))
+                val recordList = getByAuthorYear(authorString, record.year).toMutableList()
+                recordList.add(id)
+                authorYearDb.put(bytes, encode(recordList.toList()))
+            }
+        }
     }
 
     private fun encode(a: Any): ByteArray {
@@ -103,11 +154,21 @@ class DBHandler : AutoCloseable {
         return Klaxon().parseArray(String(bytes))
     }
 
+    private fun getFirstAuthorLetters(record: SemanticScholarData) : String {
+        return record.authors.joinToString(separator = ",") { author ->
+            val words = author.name.split("""\s+""".toRegex()).filter {!it.isBlank()}
+            words.joinToString { it[0].toString() }
+        }
+    }
+
     override fun close() {
         mainDb.close()
         titleDb.close()
         volPageYearDb.close()
         jpageDb.close()
+        authorVolumeDb.close()
+        authorYearDb.close()
+        authorPageDb.close()
         options.close()
     }
 }
