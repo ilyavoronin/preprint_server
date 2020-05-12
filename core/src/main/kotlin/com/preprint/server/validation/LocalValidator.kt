56610e37ee0cc906bb6d50a0a4ed931801ef83e6
Ilya Voronin <voroninilya115@gmail.com>
2020-05-12 16:05:32 +0300

    improve LocalValidator

--- core/src/main/kotlin/com/preprint/server/validation/LocalValidator.kt
+++ core/src/main/kotlin/com/preprint/server/validation/LocalValidator.kt
@@ -6,8 +6,11 @@ import com.preprint.server.data.Reference
 import com.preprint.server.validation.database.Config
 import com.preprint.server.validation.database.DBHandler
 import com.preprint.server.validation.database.UniversalData
+import org.apache.logging.log4j.kotlin.logger
+import java.lang.Math.abs
 
 object LocalValidator : Validator, AutoCloseable {
+    val logger = logger()
     val dbHandler = DBHandler(Config.config["validation_db_path"].toString())
 
     override fun validate(ref: Reference) {
@@ -26,18 +29,20 @@ object LocalValidator : Validator, AutoCloseable {
             }
         }
 
+
         if (ref.authors.size >= 2) {
             val authString = dbHandler.getFirstAuthorLetters(ref.authors.map {it.name})
+            /*
             if (!ref.volume.isNullOrBlank()) {
                 records.addAll(dbHandler.getByAuthorVolume(authString, ref.volume!!))
             }
+             */
 
             if (ref.firstPage != null) {
                 records.addAll(dbHandler.getByAuthorPage(authString, ref.firstPage!!))
             }
         }
 
-        println(records.size)
 
         records.forEach {
             if (check(ref, it)) {
@@ -50,40 +55,45 @@ object LocalValidator : Validator, AutoCloseable {
     private fun check(ref: Reference, record: UniversalData): Boolean {
         val refstr = ref.rawReference
         var score = 4
+        if (ref.year != null && record.year != null && abs(record.year!! - ref.year!!) > 1) {
+            return false
+        }
+
         if (record.year != null
                 && record.year != ref.year
-                && !refstr.contains(record.year.toString())
         ) {
             score -= 1
         }
 
         if (record.journalVolume != null
                 && record.journalVolume != ref.volume
-                && !refstr.contains(record.journalVolume!!)
         ) {
             score -= 1
         }
 
         if (record.firstPage != null
                 && record.firstPage != ref.firstPage
-                && !refstr.contains(record.firstPage!!.toString())
         ) {
             score -= 1
         }
 
         //checking authors
-        if (record.authors.any {author ->
-                    val longestPart = author.name.split("""\s""".toRegex()).filter { it.isNotBlank() }.maxBy { it.length }
-                    if (longestPart != null) {
-                        return@any !refstr.contains(longestPart)
-                    }
-                    else {
-                        return@any false
-                    }
+        if (!record.authors.all {author ->
+                val authorParts = author.name.split("""\s""".toRegex()).filter { it.isNotBlank() }
+                val longestPart = authorParts.maxBy { it.length }
+                if (longestPart != null) {
+                    return@all Algorithms.findLCS(refstr, longestPart).length + 1 >= longestPart.length
+                            || (authorParts.last().all {it.isLetter()} && authorParts.last().length > 1 &&
+                            Algorithms.findLCS(refstr, authorParts.last()).length + 1 >= authorParts.last().length)
                 }
+                else {
+                    return@all false
+                }
+            }
         ) {
-            score -= 1
+            score -= 2
         }
+
 
         if (score < 2) {
             return false
@@ -108,7 +118,7 @@ object LocalValidator : Validator, AutoCloseable {
             }
         }
 
-        if (record.title.isNullOrBlank()) {
+        if (!record.title.isNullOrBlank()) {
             val t = Algorithms.findLCS(refstr, record.title!!).length
             if (t.toDouble() / record.title!!.length.toDouble() > 0.9) {
                 score += 2
