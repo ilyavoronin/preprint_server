d564806c0b803013bcde8fe4521f5aaa6d3266c2
Ilya Voronin <voroninilya115@gmail.com>
2020-05-16 00:47:49 +0300

    add shadowJar task for arxiv-s3

--- arxiv-s3/build.gradle.kts
+++ arxiv-s3/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     java
     kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "com.preprint.server"
@@ -22,6 +25,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api-kotlin:1.0.0")
     compile("org.apache.commons:commons-compress:1.20")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+    implementation("net.sf.jopt-simple:jopt-simple:6.0-alpha-3")
     compile(project(":core"))
     testCompile("junit", "junit", "4.12")
 }
@@ -35,5 +39,20 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("arxiv-s3")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "ArxivS3MainKt"))
+        }
+        isZip64 = true
+    }
+}
+
+tasks {
+    build {
+        dependsOn("shadowJar")
     }
 }
---
+++ arxiv-s3/src/main/kotlin/ArxivS3Main.kt
@@ -0,0 +1,81 @@
+import com.preprint.server.arxivs3.ArxivS3Collector
+import com.preprint.server.core.neo4j.DatabaseHandler
+import com.preprint.server.core.ref.CustomReferenceExtractor
+import com.preprint.server.core.ref.GrobidReferenceExtractor
+import com.preprint.server.core.ref.ReferenceExtractor
+import com.preprint.server.core.validation.ArxivValidator
+import com.preprint.server.core.validation.CrossRefValidator
+import com.preprint.server.core.validation.LocalValidator
+import joptsimple.OptionParser
+import java.io.IOException
+
+fun main(args: Array<String>) {
+    val optionParser = OptionParser()
+    optionParser.accepts("download-only")
+    optionParser.accepts("ref-extractor").withRequiredArg()
+    optionParser.accepts("validators").withRequiredArg()
+    optionParser.accepts("mpd").withRequiredArg()
+    optionParser.accepts("threads").withRequiredArg()
+
+    val options = optionParser.parse(*args)
+
+    val downloadOnlyMode = options.has("download-only")
+
+
+    var refExtractor: ReferenceExtractor = CustomReferenceExtractor
+    if (options.has("ref-extractor")) {
+        when (options.valueOf("ref-extractor").toString()) {
+            "g" -> refExtractor = GrobidReferenceExtractor
+            "c" -> refExtractor = CustomReferenceExtractor
+            else -> throw IOException("Wrong argument for --ref-extractor")
+        }
+    }
+
+    var validators = mutableListOf(LocalValidator, ArxivValidator)
+    if (options.has("validators")) {
+        validators = mutableListOf()
+        options.valueOf("validators").toString().forEach { c ->
+            when (c) {
+                'l' -> validators.add(LocalValidator)
+                'c' -> validators.add(CrossRefValidator)
+                'a' -> validators.add(ArxivValidator)
+                else -> throw IOException("Wrong argument for --validators")
+            }
+        }
+    }
+
+    val maxParallelDownload = if (options.has("mpd")) {
+        options.valueOf("mpd").toString().toInt()
+    } else {
+        10
+    }
+
+    val maxThreads = if (options.has("thread")) {
+        options.valueOf("threads").toString().toInt()
+    } else {
+        -1
+    }
+
+
+    val dbHandler = if (!downloadOnlyMode) DatabaseHandler(
+        ArxivS3Config.config["neo4j_url"].toString(),
+        ArxivS3Config.config["neo4j_port"].toString(),
+        ArxivS3Config.config["neo4j_user"].toString(),
+        ArxivS3Config.config["neo4j_password"].toString()
+    ) else null
+
+    Runtime.getRuntime().addShutdownHook(object : Thread() {
+        override fun run() {
+            dbHandler?.close()
+            LocalValidator.close()
+        }
+    })
+
+    ArxivS3Collector.beginBulkDownload(
+        dbHandler,
+        refExtractor,
+        validators,
+        maxParallelDownload,
+        maxThreads
+    )
+}
